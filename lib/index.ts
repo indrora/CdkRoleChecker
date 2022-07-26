117fc2166043b53fdb3d92258701d7041603ac5b
Morgan Gangwere <gangwere@amazon.com>
2022-07-25 17:52:47 -0700

    First commit

--- lib/index.ts
+++ lib/index.ts
@@ -1,20 +1,94 @@
-import { Construct } from 'constructs';
+import { Annotations, Aspects, aws_iam, IAspect } from 'aws-cdk-lib';
+import { ImagePullPrincipalType } from 'aws-cdk-lib/aws-codebuild';
+import { CfnRole, Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { isMaster } from 'cluster';
+import { Construct, IConstruct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export interface CdkRoleCheckerProps {
   // Define construct properties here
+  /**
+   * Banned Services which must not be used
+   *
+   * @default - none, all services can be called by the role
+   */
+  readonly bannedServices?: string[]
+  /**
+   * Roles may use the following AWS calls (in IAM-style service:call format)
+   * @default - noe, all 
+   */
+  readonly allowList?: string[]
+  /**
+   * 
+   * Roles may never use any of these AWS calls (in iam-style service:call format)
+   * @default - None
+   */
+  readonly denyList?: string[]
+
+  readonly allowPrincipal?: string[]
+
+  /**
+   * Ban wildcard statements ("iam:*" and similar)
+   * @default false
+   */
+  readonly noWildcardCalls?: Boolean
+
+  readonly noWildcardResources?: Boolean
 }
 
-export class CdkRoleChecker extends Construct {
 
-  constructor(scope: Construct, id: string, props: CdkRoleCheckerProps = {}) {
-    super(scope, id);
 
-    // Define construct contents here
+export class CdkRoleChecker implements IAspect {
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkRoleCheckerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  private allowedCalls?: string[]
+  private deniedCalls?: string[]
+  private banServices?: string[]
+  private allowPrincipals?: string[]
+  private banWildcardCalls: Boolean
+
+  constructor(props: CdkRoleCheckerProps = {}) {
+
+    this.allowedCalls = props.allowList
+    this.allowPrincipals = props.allowPrincipal;
+
+    this.banWildcardCalls = props.noWildcardCalls ?? false
+
   }
+  visit(node: IConstruct): void {
+    if (node instanceof aws_iam.Role) {
+      
+      const cfnRole = (node.node.findChild('Resource') as CfnRole);
+      for (const policy of (cfnRole.policies as CfnRole.PolicyProperty[])) {
+        for (const pStatement of policy.policyDocument.statements as PolicyStatement[]) {
+          if (!this.check(pStatement, node)) {
+          Annotations.of(node).addError("Role does not conform to requirements")
+        }
+      }
+    }
+  }
+}
+
+private check(statement: PolicyStatement, role: aws_iam.Role): Boolean {
+
+    // Deny statements are out of scope here, as they're default. 
+    if (statement.effect == aws_iam.Effect.DENY) {
+      return true;
+    }
+
+    // Easy first pass: Are there any wildcards and should we care? 
+    if (this.banWildcardCalls) {
+      if(statement.actions.some( (x) => {x.endsWith('*')} )) 
+      {
+        console.error("Foundwildcard: {}")
+        Annotations.of(role).addError("Found wildcard!")
+      }
+    }
+
+    if (this.allowedCalls) {
+      // We should check that the calls being made by the 
+    }
+
+    return true
+  }
+
 }
--- test/cdk-role-checker.test.ts
+++ test/cdk-role-checker.test.ts
@@ -1,18 +1,46 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkRoleChecker from '../lib/index';
+import * as cdk from "aws-cdk-lib";
+import { Aspects } from "aws-cdk-lib";
+import {Template} from "aws-cdk-lib/assertions";
+import * as iam from "aws-cdk-lib/aws-iam"
+import { addCustomSynthesis } from "aws-cdk-lib/core/lib/private/synthesis";
+import * as CdkRoleChecker from "../lib/index";
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/index.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//   const stack = new cdk.Stack(app, "TestStack");
-//   // WHEN
-//   new CdkRoleChecker.CdkRoleChecker(stack, 'MyTestConstruct');
-//   // THEN
-//   const template = Template.fromStack(stack);
+test("asdfasdf bananas", () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, "TestStack");
+  
+      // 👇 Create a Policy Document (Collection of Policy Statements)
+      const filterLogEvents = new iam.PolicyDocument({
+        statements: [
+          new iam.PolicyStatement({
+            resources: ['arn:aws:logs:*:*:log-group:/aws/lambda/*'],
+            actions: ['logs:FilterLogEvents','logs:touch*'],
+            // 👇 Default for `effect` is ALLOW
+            effect: iam.Effect.ALLOW,
+          }),
+        ],
+      });
+  
+      // 👇 Create role, to which we'll attach our Policies
+      const role = new iam.Role(stack, 'example-iam-role', {
+        assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+        description: 'An example IAM role in AWS CDK',
+        inlinePolicies: {
+          // 👇 attach the Policy Document as inline policies
+          FilterLogEvents: filterLogEvents,
+        },
+      });
+    
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  const checker = new CdkRoleChecker.CdkRoleChecker({
+    'noWildcardCalls':true
+  });
+  Aspects.of(stack).add(checker);
+  const template = Template.fromStack(stack);
+
+  const end  = app.synth()
+
+  
 });
