e227a69216854ef4cc1b321f70c59bd3a291851b
Morgan Gangwere <gangwere@amazon.com>
2022-07-28 15:07:28 -0700

    Clean up logic and reduce scope, add tests

--- lib/index.ts
+++ lib/index.ts
@@ -1,40 +1,30 @@
-import { Annotations, Aspects, aws_iam, IAspect } from 'aws-cdk-lib';
-import { ImagePullPrincipalType } from 'aws-cdk-lib/aws-codebuild';
-import { CfnRole, Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { isMaster } from 'cluster';
-import { Construct, IConstruct } from 'constructs';
-import minimatch
+import { Annotations, aws_iam, IAspect } from 'aws-cdk-lib';
+import { CfnRole,  PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { IConstruct } from 'constructs';
+
+const minimatch = require("minimatch");
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export interface CdkRoleCheckerProps {
-  // Define construct properties here
-  /**
-   * Banned Services which must not be used
-   *
-   * @default - none, all services can be called by the role
-   */
-  readonly bannedServices?: string[]
+
   /**
    * Roles may use the following AWS calls (in IAM-style service:call format)
-   * @default - noe, all 
+   * @default - None: All calls are allowed.
    */
   readonly allowList?: string[]
   /**
    * 
    * Roles may never use any of these AWS calls (in iam-style service:call format)
-   * @default - None
+   * @default - None, no calls are denied explicitly.
    */
   readonly denyList?: string[]
-
-  readonly allowPrincipal?: string[]
 
   /**
    * Ban wildcard statements ("iam:*" and similar)
    * @default false
    */
-  readonly noWildcardCalls?: Boolean
-
-  readonly noWildcardResources?: Boolean
+  readonly banWildcards?: Boolean
 }
 
 
@@ -43,33 +33,39 @@ export class CdkRoleChecker implements IAspect {
 
   private allowedCalls?: string[]
   private deniedCalls?: string[]
-  private banServices?: string[]
-  private allowPrincipals?: string[]
   private banWildcardCalls: Boolean
+  private visitedNodes: string[]
 
   constructor(props: CdkRoleCheckerProps = {}) {
 
     this.allowedCalls = props.allowList
-    this.allowPrincipals = props.allowPrincipal;
-
-    this.banWildcardCalls = props.noWildcardCalls ?? false
+    this.deniedCalls  = props.denyList
+    this.banWildcardCalls = props.banWildcards ?? false
+    this.visitedNodes = []
 
   }
   visit(node: IConstruct): void {
     if (node instanceof aws_iam.Role) {
 
+      const nodepath = node.node.path
+
+      if(this.visitedNodes.includes(nodepath)) {
+        return;
+      }
       const cfnRole = (node.node.findChild('Resource') as CfnRole);
       for (const policy of (cfnRole.policies as CfnRole.PolicyProperty[])) {
         for (const pStatement of policy.policyDocument.statements as PolicyStatement[]) {
           if (!this.check(pStatement, node)) {
-            Annotations.of(node).addError("Role does not conform to requirements")
+            Annotations.of(node).addInfo("Role does not conform to requirements");
           }
         }
       }
+
+      this.visitedNodes.push(nodepath);
     }
   }
 
-  public check(statement: PolicyStatement, role: aws_iam.Role): Boolean {
+  public check(statement: PolicyStatement, role: aws_iam.Role)  {
 
     // Deny statements are out of scope here, as they're default. 
     if (statement.effect == aws_iam.Effect.DENY) {
@@ -78,41 +74,48 @@ export class CdkRoleChecker implements IAspect {
 
     // Easy first pass: Are there any wildcards and should we care? 
     if (this.banWildcardCalls) {
-      if (statement.actions.some((x) => { x.endsWith('*') })) {
-        console.error("Found wildcard: " + statement.actions.find((x) => { x.endsWith('*') }))
-        Annotations.of(role).addError("Found wildcard!");
+      // If wildcards are banned, 
+      const maybeWildcards = statement.actions.filter( ( s ) => s.endsWith("*"))
+      for(const wildAction of maybeWildcards) {
+        Annotations.of(role).addError("Wildcard used: "+wildAction);
       }
     }
+  
+    // Second pass: Wildcards must have their wildcard at the end, not the middle. 
+    // While IAM doesn't technically allow it, we're going to enforce well-formed wildcards:
 
-    if (this.allowedCalls) {
-      // Check each call to make sure that it is allowed.
-      for (const checkCall of statement.actions) {
-        // If it's already there, no problem. 
-        if (this.allowedCalls.includes(checkCall)) {
-          if (checkCall.indexOf('*') >= 0 && this.banWildcardCalls) {
-            Annotations.of(role).addError("Banned wildcard found: "+checkCall);
+    if(statement.actions.some((a) => {a.indexOf('*') != -1 && !a.endsWith("*")})) {
+      Annotations.of(role).addWarning("You have described a wildcard with a suffix and prefix. This is likely not invalid but may lead to unintended consequences.");
+    }
+
+    if(this.allowedCalls) {
+      // Check each call in the statement to make sure it's in the allowed list
+      for(const checkCall of statement.actions) {
+        if(!checkCall.endsWith("*") && !this.allowedCalls.some((x)=> minimatch(checkCall, x))) {
+          Annotations.of(role).addError("Role "+role.roleName+" contains non-cleared permission "+checkCall)
+        } else if(checkCall.endsWith("*") && !this.banWildcardCalls) {
+          // Wildcards are special. Wildcards must be a subset of *another* wildcard. 
+          const possibleSuperset = this.allowedCalls.filter((s)=>s.endsWith("*"));
+          
+          const trimmed = checkCall.substring(0,checkCall.length-1);
+
+          if(!possibleSuperset.includes(checkCall) && !possibleSuperset.some( (x) => minimatch(trimmed, x) )) {
+            // There's no wildcard that matches it, or no direct literal.
+            console.log("Adding error for "+checkCall)
+            Annotations.of(role).addError("Role contains a wildcard with greater scope than allowed");
           }
-          continue;
         }
-        // Check if it's a wildcard
-        if (checkCall.indexOf('*') >= 0) {
-          // if we ban wildcards, mark it and continue
-          if (this.banWildcardCalls) {
-            // Not okay
-            Annotations.of(role).addError("Banned wildcard found: "+checkCall)
-            continue;
-          }
-          
-          const unwildcard = checkCall.replace('*', '')
-          
-          // 
-          
+      }
+    } else if(this.deniedCalls) {
+      for(const denycall of this.deniedCalls) {
+        
+        // No statement actions should be or resolve to denycall
+        const maybeBanned = statement.actions.filter( (x) => minimatch(x, denycall) || ( x.endsWith('*') && minimatch(denycall, x)) ) ;
+        if(maybeBanned.length > 0 ) {
+          Annotations.of(role).addError("Role contains denied calls: "+maybeBanned.join(',') )
         }
       }
     }
-
-
-    return true;
   }
 
 }
--- test/cdk-role-checker.test.ts
+++ test/cdk-role-checker.test.ts
@@ -1,46 +1,180 @@
 import * as cdk from "aws-cdk-lib";
-import { Aspects } from "aws-cdk-lib";
-import {Template} from "aws-cdk-lib/assertions";
-import * as iam from "aws-cdk-lib/aws-iam"
-import { addCustomSynthesis } from "aws-cdk-lib/core/lib/private/synthesis";
+import {Annotations, Aspects, Stack} from "aws-cdk-lib";
+import {assertions} from "aws-cdk-lib";
+import {Match, Template} from "aws-cdk-lib/assertions";
+import {GitHubEnterpriseSourceCredentials} from "aws-cdk-lib/aws-codebuild";
+import {FireLensLogDriver} from "aws-cdk-lib/aws-ecs";
+import * as iam from "aws-cdk-lib/aws-iam";
+import {addCustomSynthesis} from "aws-cdk-lib/core/lib/private/synthesis";
 import * as CdkRoleChecker from "../lib/index";
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/index.ts
-test("asdfasdf bananas", () => {
+
+function summonTestRole(stack : cdk.Stack, actions : string[]): iam.Role {
+  const mPolicyDocument = new iam.PolicyDocument({
+    statements: [new iam.PolicyStatement({resources: ["arn:aws:logs:*:*:log-group:/aws/testing/*"], actions: actions, effect: iam.Effect.ALLOW})]
+  });
+
+  const role = new iam.Role(stack, "example-iam-role", {
+    assumedBy: new iam.ServicePrincipal("example-service.amazonaws.com"),
+    description: "Role for testing",
+    inlinePolicies: {
+      FilterLogEvents: mPolicyDocument
+    }
+  });
+
+  return role;
+}
+
+function getErrors(stack : cdk.Stack): any {
+  return assertions.Annotations.fromStack(stack).findError("*", Match.anyValue());
+}
+
+test("AllowList with clean set", () => {
   const app = new cdk.App();
   const stack = new cdk.Stack(app, "TestStack");
-  
-      // 👇 Create a Policy Document (Collection of Policy Statements)
-      const filterLogEvents = new iam.PolicyDocument({
-        statements: [
-          new iam.PolicyStatement({
-            resources: ['arn:aws:logs:*:*:log-group:/aws/lambda/*'],
-            actions: ['logs:FilterLogEvents','logs:touch*'],
-            // 👇 Default for `effect` is ALLOW
-            effect: iam.Effect.ALLOW,
-          }),
-        ],
-      });
-  
-      // 👇 Create role, to which we'll attach our Policies
-      const role = new iam.Role(stack, 'example-iam-role', {
-        assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-        description: 'An example IAM role in AWS CDK',
-        inlinePolicies: {
-          // 👇 attach the Policy Document as inline policies
-          FilterLogEvents: filterLogEvents,
-        },
-      });
-    
+  const role = summonTestRole(stack, ["test:test"]);
 
-  const checker = new CdkRoleChecker.CdkRoleChecker({
-    'noWildcardCalls':true
-  });
-  Aspects.of(stack).add(checker);
-  const template = Template.fromStack(stack);
+  const checker = new CdkRoleChecker.CdkRoleChecker({allowList: ["test:test"]});
+  Aspects.of(role).add(checker);
 
-  const end  = app.synth()
+  const end = app.synth();
 
-  
+  const errors = getErrors(stack);
+  expect(errors.length).toBe(0);
 });
+
+test("AllowList with wildcard", () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, "TestStack");
+  const role = summonTestRole(stack, ["test:test"]);
+
+  const checker = new CdkRoleChecker.CdkRoleChecker({allowList: ["test:*"]});
+  Aspects.of(role).add(checker);
+
+  const end = app.synth();
+
+  const errors = getErrors(stack);
+  expect(errors.length).toBe(0);
+});
+
+test("AllowList but overscoped subset", () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, "TestStack");
+  const role = summonTestRole(stack, ["test:get*"]);
+
+  const checker = new CdkRoleChecker.CdkRoleChecker({allowList: ["test:getLeft*"]});
+  Aspects.of(role).add(checker);
+  const end = app.synth();
+  const errors = getErrors(stack);
+  expect(errors.length).toBe(1);
+});
+
+test("Denylist with single item", () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, "TestStack");
+  const role = summonTestRole(stack, ["test:test"]);
+
+  const checker = new CdkRoleChecker.CdkRoleChecker({denyList: ["test:test"]});
+  Aspects.of(role).add(checker);
+  const end = app.synth();
+
+  const errors = getErrors(stack);
+  expect(errors.length).toBe(1);
+});
+
+test("Denylist is subset", () => {
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, "TestStack");
+    const role = summonTestRole(stack, ["test:test", "test:open", "test:cheese"]);
+  
+    const checker = new CdkRoleChecker.CdkRoleChecker({denyList: ["test:test"]});
+    Aspects.of(role).add(checker);
+    const end = app.synth();
+  
+    const errors = getErrors(stack);
+    expect(errors.length).toBe(1);
+  });
+
+  test("Denylist with wildcard in role", () => {
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, "TestStack");
+    const role = summonTestRole(stack, ["test:t*"]);
+  
+    const checker = new CdkRoleChecker.CdkRoleChecker({denyList: ["test:test"]});
+    Aspects.of(role).add(checker);
+    const end = app.synth();
+  
+    const errors = getErrors(stack);
+    expect(errors.length).toBe(1);
+  });
+
+  test("Denylist with wildcard in denied list", () => {
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, "TestStack");
+    const role = summonTestRole(stack, ["test:test"]);
+  
+    const checker = new CdkRoleChecker.CdkRoleChecker({denyList: ["test:t*"]});
+    Aspects.of(role).add(checker);
+    const end = app.synth();
+  
+    const errors = getErrors(stack);
+    expect(errors.length).toBe(1);
+  });
+
+
+
+test("DenyList but everything is OK", () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, "TestStack");
+  const role = summonTestRole(stack, ["test:test"]);
+  const checker = new CdkRoleChecker.CdkRoleChecker({denyList: ["test:getBar"]});
+  Aspects.of(role).add(checker);
+  const end = app.synth();
+
+  const errors = getErrors(stack);
+  expect(errors.length).toBe(0);
+});
+
+test("DenyList with wildcard", () => {
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, "TestStack");
+    const role = summonTestRole(stack, ["test:test"]);
+  
+  const checker = new CdkRoleChecker.CdkRoleChecker({denyList: ["test:*"]});
+  Aspects.of(role).add(checker);
+
+  const end = app.synth();
+
+  const errors = getErrors(stack);
+  expect(errors.length).toBe(1);
+});
+
+test("AllowList, clean, but with wildcards banned", () => {
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, "TestStack");
+    const role = summonTestRole(stack, ["test:test"]);
+  
+    const checker = new CdkRoleChecker.CdkRoleChecker({allowList: ["test:test"],banWildcards:true});
+    Aspects.of(role).add(checker);
+    const end = app.synth();
+  
+    const errors = getErrors(stack);
+    expect(errors.length).toBe(0);
+  });
+  
+  test("AllowList, but with wildcards banned.", () => {
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, "TestStack");
+    const role = summonTestRole(stack, ["test:test", "test:generate*"]);
+  
+    const checker = new CdkRoleChecker.CdkRoleChecker({allowList: ["test:test"], banWildcards:true});
+    Aspects.of(role).add(checker);
+    const end = app.synth();
+  
+    const errors = getErrors(stack);
+    console.log(errors);
+    expect(errors.length).toBe(1);
+  });
+  
